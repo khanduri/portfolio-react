b9936b3b07fe130a5972499363fe373d368751de
Prashant Khanduri <prashant.khanduri@gmail.com>
2016-03-04 00:29:41 -0800

    Updating navigation

--- static/scripts/jsx/main.jsx
+++ static/scripts/jsx/main.jsx
@@ -9,7 +9,7 @@ var NavigationComponent = React.createClass({
                     <i className="fa fa-bars"></i>
                 </button>
                 <a className="navbar-brand page-scroll" href="#page-top">
-                    <i className="fa fa-play-circle"></i>  <span className="light">Start</span> Bootstrap
+                    <i className="fa fa-play-circle"></i>  <span className="light">HOME</span>
                 </a>
             </div>
 
@@ -17,7 +17,10 @@ var NavigationComponent = React.createClass({
                 <ul className="nav navbar-nav">
                     <li className="hidden"><a href="#page-top"></a></li>
                     <li><a className="page-scroll" href="#about">About</a></li>
-                    <li><a className="page-scroll" href="#download">Download</a></li>
+                    <li><a className="page-scroll" href="#learn">Learn</a></li>
+                    <li><a className="page-scroll" href="#experience">Experience</a></li>
+                    <li><a className="page-scroll" href="#education">Education</a></li>
+                    <li><a className="page-scroll" href="#projects">Activities</a></li>
                     <li><a className="page-scroll" href="#contact">Contact</a></li>
                 </ul>
             </div>
