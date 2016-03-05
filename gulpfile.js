da23d952b8d37706b6cdc8ce5a2a2a7bb9a526e6
Prashant Khanduri <prashant.khanduri@gmail.com>
2016-03-04 22:10:50 -0800

    adding 404 page support

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,6 @@
 
 var gulp = require('gulp'),
     size = require('gulp-size'),
-    // browserify = require('browserify'),
-    // reactify = require('reactify'),
-    // jsx = require('gulp-jsx'),
     react = require('gulp-react'),
     concat = require('gulp-concat'),
     clean = require('gulp-clean'),
@@ -16,40 +13,34 @@ var gulp = require('gulp'),
 // Tasks
 gulp.task('default', function(){
   gulp.start('transform');
-  gulp.watch('./sass/**/*.scss', ['sass']);
-  gulp.watch('static/scripts/jsx/*.jsx', ['transform']);
+  gulp.watch('app/static/styles/scss/*.scss', ['sass']);
+  gulp.watch('app/static/scripts/jsx/*.jsx', ['transform']);
 });
 
 gulp.task('build', ['clean'], function(){
-  return gulp.src('static/scripts/jsx/*.jsx')
+  return gulp.src('app/static/scripts/jsx/*.jsx')
     .pipe(react({harmony: false,}))
     .pipe(uglify())
     .pipe(concat('main.js'))
-    .pipe(gulp.dest('static/scripts/js/compiled'))
+    .pipe(gulp.dest('app/static/scripts/js/compiled'))
     .pipe(size());
 });
 
 gulp.task('transform', ['clean'], function(){
-  // The transformer is not working for me .. I'm just using the client side transformer
-  return gulp.src('static/scripts/jsx/*.jsx')
-    // .pipe(browserify({transform: [reactify]}))
-    // .pipe(jsx({factory: 'React.createClass'}))
+  return gulp.src('app/static/scripts/jsx/*.jsx')
     .pipe(react({harmony: false,}))
     .pipe(concat('main.js'))
-    .pipe(gulp.dest('static/scripts/js/compiled'))
+    .pipe(gulp.dest('app/static/scripts/js/compiled'))
     .pipe(size());
 });
 
 gulp.task('clean', function(){
-  return gulp.src(
-      [
-        'static/scripts/js/compiled'
-      ], {read: false})
+  return gulp.src(['app/static/scripts/js/compiled'], {read: false})
     .pipe(clean());
 });
 
 gulp.task('sass', function () {
-  return gulp.src('static/styles/scss/main.scss')
+  return gulp.src('app/static/styles/scss/main.scss')
     .pipe(sass.sync().on('error', sass.logError))
-    .pipe(gulp.dest('static/styles/css'));
+    .pipe(gulp.dest('app/static/styles/css'));
 });
